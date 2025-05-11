565131ce257091783f87b1148f63e979453d21ab
Renan Pollo Benelli <renansk18@outlook.com>
2025-05-11 12:22:19 -0300

    feat: add CORS config

--- backend/interfaces/http/App.js
+++ backend/interfaces/http/App.js
@@ -1,10 +1,18 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use(authRoutes);
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
